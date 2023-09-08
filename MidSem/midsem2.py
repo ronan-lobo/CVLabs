066246c1420eee6d44bbaa21bd8761a0f68fd1b3
ronan-lobo <142533335+ronan-lobo@users.noreply.github.com>
2023-09-08 11:38:36 +0530

    Add files via upload

--- MidSem/midsem2.py
+++ MidSem/midsem2.py
@@ -2,38 +2,34 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-im1 = cv2.imread('Resources\\image (19).jpg', 0)
+im = cv2.imread('Resources\\image (19).jpg')
+gauss = cv2.GaussianBlur(im, (21, 21), 0)
 
-plt.subplot(2, 3, 1)
-plt.imshow(cv2.cvtColor(im1, cv2.COLOR_BGR2RGB))
-plt.subplot(2, 3, 4)
-hist = np.histogram(np.array(im1).ravel(), bins=256, range=[0, 256])
+minval, maxval, mincoord, maxcoord = cv2.minMaxLoc(cv2.cvtColor(gauss, cv2.COLOR_BGR2GRAY))
+
+gray_img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+windowName = 'Thresholded Image'
+
+hist = np.histogram(np.array(im).ravel(), bins=256, range=[0, 256])
 x, y = hist[1][:-1], hist[0]
 y = y / np.sum(y)
 y = np.cumsum(y)
-plt.bar(x, y)
 
-im2 = cv2.imread('Resources\\image (87).jpg', 0)
+vals = np.where(y > 0.8)
+value = vals[0][0]
 
-plt.subplot(2, 3, 2)
-plt.imshow(cv2.cvtColor(im2, cv2.COLOR_BGR2RGB))
-plt.subplot(2, 3, 5)
-hist = np.histogram(np.array(im2).ravel(), bins=256, range=[0, 256])
-x, y = hist[1][:-1], hist[0]
-y = y / np.sum(y)
-y = np.cumsum(y)
-plt.bar(x, y)
+ret, mask = cv2.threshold(gray_img, value, 255, cv2.THRESH_BINARY)
+mask = np.array(mask)
+x, y = maxcoord
+mask[y-75: y+75, x-75: x+75] = 0
+out_img = np.array(im)
+mask_coords = np.where(mask == 255)
+for i, j in zip(mask_coords[0], mask_coords[1]):
+    out_img[i, j, :] = [255, 127, 0]
+cv2.imshow(windowName, out_img)
 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-im3 = cv2.imread('Resources\\image (95).jpg', 0)
-
-plt.subplot(2, 3, 3)
-plt.imshow(cv2.cvtColor(im3, cv2.COLOR_BGR2RGB))
-plt.subplot(2, 3, 6)
-hist = np.histogram(np.array(im3).ravel(), bins=256, range=[0, 256])
-x, y = hist[1][:-1], hist[0]
-y = y / np.sum(y)
-y = np.cumsum(y)
-plt.bar(x, y)
-plt.show()
 
